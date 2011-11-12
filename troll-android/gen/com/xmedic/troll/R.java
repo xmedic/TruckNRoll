332f9bcf170f15f05b318a5be93f6b30ac7c3b2b
Vincentas Vienozinskis <vincnetas@gmail.com>
2011-11-12 21:42:51 +0200

    success and fail screens

--- troll-android/gen/com/xmedic/troll/R.java
+++ troll-android/gen/com/xmedic/troll/R.java
@@ -31,35 +31,37 @@ public final class R {
         public static final int xmediclogo=0x7f02000e;
     }
     public static final class id {
-        public static final int button1=0x7f060012;
-        public static final int button2=0x7f060010;
-        public static final int button3=0x7f060011;
-        public static final int button4=0x7f060013;
-        public static final int buttonLevel1=0x7f060003;
-        public static final int buttonLevel2=0x7f060004;
-        public static final int buttonLevel3=0x7f060005;
-        public static final int buttonLevel4=0x7f060007;
-        public static final int buttonLevel5=0x7f060008;
-        public static final int buttonLevel6=0x7f060009;
-        public static final int imageView1=0x7f06000d;
-        public static final int imageView2=0x7f06000e;
-        public static final int linearLayout1=0x7f060001;
-        public static final int linearLayout2=0x7f060002;
-        public static final int linearLayout3=0x7f060006;
-        public static final int map=0x7f06000a;
+        public static final int button1=0x7f060009;
+        public static final int button2=0x7f060007;
+        public static final int button3=0x7f060008;
+        public static final int button4=0x7f06000a;
+        public static final int buttonLevel1=0x7f06000e;
+        public static final int buttonLevel2=0x7f06000f;
+        public static final int buttonLevel3=0x7f060010;
+        public static final int buttonLevel4=0x7f060012;
+        public static final int buttonLevel5=0x7f060013;
+        public static final int buttonLevel6=0x7f060014;
+        public static final int imageView1=0x7f060004;
+        public static final int imageView2=0x7f060005;
+        public static final int linearLayout1=0x7f06000c;
+        public static final int linearLayout2=0x7f06000d;
+        public static final int linearLayout3=0x7f060011;
+        public static final int map=0x7f060001;
         public static final int movingtruck=0x7f060017;
-        public static final int progressBar1=0x7f06000b;
+        public static final int progressBar1=0x7f060002;
         public static final int relativeLayout1=0x7f060000;
-        public static final int relativeLayout2=0x7f06000c;
-        public static final int relativeLayout3=0x7f06000f;
-        public static final int targetcity=0x7f060014;
+        public static final int relativeLayout2=0x7f060003;
+        public static final int relativeLayout3=0x7f060006;
+        public static final int targetcity=0x7f06000b;
         public static final int xmediclogo=0x7f060016;
         public static final int yo=0x7f060015;
     }
     public static final class layout {
-        public static final int homescreen=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int splashscreen=0x7f030002;
+        public static final int failscreen=0x7f030000;
+        public static final int homescreen=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int splashscreen=0x7f030003;
+        public static final int successscreen=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
---
+++ troll-android/src/com/xmedic/troll/FailScreenActivity.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.xmedic.troll;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * @author vincentas
+ *
+ */
+public class FailScreenActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        setContentView(R.layout.failscreen);
+	}
+
+	
+}
