36ef140429371b076f784b75205dad99ad99faf4
Vincentas Vienozinskis <vincnetas@gmail.com>
2011-11-12 21:29:48 +0200

    gradientas

--- troll-android/gen/com/xmedic/troll/R.java
+++ troll-android/gen/com/xmedic/troll/R.java
@@ -19,15 +19,16 @@ public final class R {
         public static final int background=0x7f020002;
         public static final int backrepeat=0x7f020003;
         public static final int disabled=0x7f020004;
-        public static final int hudleft=0x7f020005;
-        public static final int hudright=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int icon=0x7f020008;
-        public static final int lietuvosplikaszemelapis=0x7f020009;
-        public static final int normal=0x7f02000a;
-        public static final int pressed=0x7f02000b;
-        public static final int rounded_corner_box=0x7f02000c;
-        public static final int xmediclogo=0x7f02000d;
+        public static final int gradient=0x7f020005;
+        public static final int hudleft=0x7f020006;
+        public static final int hudright=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int icon=0x7f020009;
+        public static final int lietuvosplikaszemelapis=0x7f02000a;
+        public static final int normal=0x7f02000b;
+        public static final int pressed=0x7f02000c;
+        public static final int rounded_corner_box=0x7f02000d;
+        public static final int xmediclogo=0x7f02000e;
     }
     public static final class id {
         public static final int button1=0x7f060012;
