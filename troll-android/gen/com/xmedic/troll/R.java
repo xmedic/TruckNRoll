1752327b40466d59b79f234a9cf70f720aab48af
Saulius Mačiulis <saulius.maciulis@onegoteam.com>
2011-11-12 10:24:22 +0200

    Added eclipse project

--- troll-android/gen/com/xmedic/troll/R.java
+++ troll-android/gen/com/xmedic/troll/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package xmed.troll;
+package com.xmedic.troll;
 
 public final class R {
     public static final class attr {
--- troll-android/src/com/xmedic/troll/TrucknrollAndroidActivity.java
+++ troll-android/src/com/xmedic/troll/TrucknrollAndroidActivity.java
@@ -1,9 +1,9 @@
-package xmed.troll;
+package com.xmedic.troll;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class TrucknrollActivity extends Activity {
+public class TrucknrollAndroidActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
