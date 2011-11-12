e5bd7dddf5ca7653a281f25a4301dda3e8c17f44
Saulius Mačiulis <saulius.maciulis@onegoteam.com>
2011-11-12 16:31:02 +0200

    Added x,y for city

---
+++ troll-android/assets/scripts/roads.groovy
@@ -0,0 +1,32 @@
+reader = new BufferedReader(new FileReader("LT-cities.txt"))
+writer = new FileWriter("city.txt")
+
+
+cities = {}
+linenr = 0
+reader.eachLine{ line ->
+  println line.split(";")[0] + " " + linenr
+  cities[line.split(";")[0]] = linenr
+  // add city id by lineNumber
+  writer.write("$linenr;$line\n")
+  linenr = linenr + 1
+}
+writer.close()
+reader.close()
+
+
+reader = new BufferedReader(new FileReader("LT-roads.txt"))
+writer = new FileWriter("roads.txt")
+reader.eachLine{ line ->
+
+  newLine = ""
+  for (city in line.split(";")) {
+        newLine = newLine + ";" + cities.get(city)
+  }
+
+  println newLine
+  writer.write("$newLine")
+}
+writer.close()
+reader.close()
+
