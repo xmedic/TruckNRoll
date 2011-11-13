b8e4e44e032647921d5c1e453cfcae6a51896961
Gytis Ramanauskas <gytis.ramanauskas@onegoteam.com>
2011-11-13 14:08:41 +0200

    changing dialogs without titles

--- troll-android/src/com/xmedic/troll/dialogs/FailDialog.java
+++ troll-android/src/com/xmedic/troll/dialogs/FailDialog.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import com.xmedic.troll.HomeScreenActiity;
@@ -21,6 +22,7 @@ public class FailDialog  extends Dialog implements android.view.View.OnClickList
 	 
 	public FailDialog(Context context, String levelId) {
 		super(context);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
        this.context = context;
        setTitle("No beer for today...");
        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
